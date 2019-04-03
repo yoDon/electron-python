aa731a01b1e606d34cf8c604858ac3c0143e320c
Don Alvarez <alvarezdon@outlook.com>
2019-04-02 19:27:56 -0700

    Rename folder

---
+++ electron-create-react-app-typescript-python/python/api.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask_cors import CORS
+from graphene import ObjectType, String, Schema
+from flask_graphql import GraphQLView
+from calc import calc as real_calc
+import argparse
+import os
+
+#
+# Notes on setting up a flask GraphQL server
+# https://codeburst.io/how-to-build-a-graphql-wrapper-for-a-restful-api-in-python-b49767676630
+#
+# Notes on using pyinstaller to package a flask server (discussing issues that don't come up
+# in this simple example but likely would come up in a more real application)
+# for making pyinstaller see https://mapopa.blogspot.com/2013/10/flask-and-pyinstaller-notice.html
+# and https://github.com/pyinstaller/pyinstaller/issues/1071
+# and https://elc.github.io/posts/executable-flask-pyinstaller/
+#
+
+class Query(ObjectType):
+
+#
+# IMPORTANT - There is currently nothing preventing a malicious web page
+#             running in the users web browser from making requests of this
+#             server. If you add additional code here you will need to make
+#             sure its either code that is appropriate for a malicious web
+#             page to be able to run (like the calculator example below) or
+#             that you wrap some kind of security model around the python
+#             web server before adding the code.
+#
+
+    awake = String(description="Awake")
+    def resolve_awake(self, args):
+        return "Awake"
+
+    exit = String(description="Exit", signingkey=String(required=True))
+    def resolve_exit(self, info, signingkey):
+        if signingkey != apiSigningKey:
+            return
+        os._exit(0)
+        return
+
+    hello = String(description="Hello", signingkey=String(required=True))
+    def resolve_hello(self, info, signingkey):
+        if signingkey != apiSigningKey:
+            return ""
+        return "World"
+    
+    calc = String(description="Calculator", signingkey=String(required=True), math=String(required=True))
+    def resolve_calc(self, info, signingkey, math):
+        """based on the input text, return the int result"""
+        if signingkey != apiSigningKey:
+            return ""
+        try:
+            return real_calc(math)
+        except Exception:
+            return 0.0
+    
+    echo = String(description="Echo", signingkey=String(required=True), text=String(required=True))
+    def resolve_echo(self, info, signingkey, text):
+        if signingkey != apiSigningKey:
+            return ""
+        """echo any text"""
+        return text
+
+view_func = GraphQLView.as_view("graphql", schema=Schema(query=Query), graphiql=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--apiport", type=int, default=5000)
+parser.add_argument("--signingkey", type=str, default="")
+args = parser.parse_args()
+
+apiSigningKey = args.signingkey
+
+app = Flask(__name__)
+app.add_url_rule("/graphql/", view_func=view_func)
+CORS(app) # Allows all domains to access the flask server via CORS
+
+if __name__ == "__main__":
+    app.run(port=args.apiport)
